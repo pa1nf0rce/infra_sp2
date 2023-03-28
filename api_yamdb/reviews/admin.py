4864e1ab59269e7038601edb2fccad5fd7335ae7
Роман Авраменко <roma.avramencko2012@yandex.ru>
2023-03-29 00:22:49 +0300

    Initial commit

---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,7 @@
+from reviews.models import Category, Genre, GenreTitle, Title
+from django.contrib import admin
+
+admin.register(Title)
+admin.register(Genre)
+admin.register(Category)
+admin.register(GenreTitle)
